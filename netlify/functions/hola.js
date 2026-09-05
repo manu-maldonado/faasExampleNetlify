d8d8f61c1fa5bc54a516385b410bc5fc7011a1d2
Manuel Maldonado <thekingmanuel@gmail.com>
2026-09-05 11:37:19 -0600

    agregaron areas

--- netlify/functions/hola.js
+++ netlify/functions/hola.js
@@ -1,9 +1,10 @@
 export default async (req, context) => {
   const url = new URL(req.url);
   const nombre = url.searchParams.get("nombre") || "Estudiante";
+  const area = url.searchParams.get("area") || "TI";
 
   return new Response(JSON.stringify({
-    mensaje: `¡Hola, ${nombre}! Tu función Serverless en Netlify funciona.`,
+    mensaje: `¡Hola, ${nombre}! Tu función Serverless en Netlify funciona. Eres del area ${area}`,
     fecha: new Date().toISOString()
   }), {
     status: 200,
